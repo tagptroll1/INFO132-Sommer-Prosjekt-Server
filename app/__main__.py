acdc793c707532fec85ee07abdf7c65b4c74a6f6
Thomas Petersson <thomas@petersson.priv.no>
2019-08-07 14:39:14 +0200

    feat(endpoints): questions endpoint for different types

--- app/__main__.py
+++ app/__main__.py
@@ -4,6 +4,7 @@ from app.site.manager import Manager
 from app.site.api.dropdown_endpoint import endpoints as dropdown
 from app.site.api.fill_in_endpoint import endpoints as fill_in
 from app.site.api.multi_choice_endpoint import endpoints as multi_choice
+from app.site.api.question_endpoint import endpoints as questionset
 from app.site.api.data_endpoint import endpoints as data_endpoint
 
 
@@ -20,7 +21,7 @@ parser.add_argument(
 manager = Manager()
 
 if __name__ == "__main__":
-    endpoints = (dropdown, fill_in, multi_choice, data_endpoint)
+    endpoints = (dropdown, fill_in, multi_choice, data_endpoint, questionset)
     manager.load_api_resources(endpoints)
 
     args = parser.parse_args()
